00469ae730c9cb8f27b8b20699db570b765e4fc9
lizihao159 <lizihao159@gmail.com>
2024-10-24 13:36:41 -0700

    update firebaseHelper file to handle more operations like update and delete

--- Firebase/firebaseHelper.js
+++ Firebase/firebaseHelper.js
@@ -1,12 +1,20 @@
 // firebaseHelper.js
-import { collection, onSnapshot, addDoc, query } from 'firebase/firestore';
+import { 
+  collection, 
+  onSnapshot, 
+  addDoc, 
+  query, 
+  updateDoc, 
+  deleteDoc, 
+  doc 
+} from 'firebase/firestore';
 import { database } from './firebaseSetup';
 
 // Function to listen for real-time updates
 export function listenToCollection(collectionName, callback) {
   const q = query(collection(database, collectionName));
   return onSnapshot(q, (snapshot) => {
-    const data = snapshot.docs.map(doc => ({
+    const data = snapshot.docs.map((doc) => ({
       id: doc.id,
       ...doc.data(),
     }));
@@ -20,5 +28,25 @@ export async function addDocument(collectionName, data) {
     await addDoc(collection(database, collectionName), data);
   } catch (error) {
     console.error('Error adding document: ', error);
+  }
+}
+
+// Function to update a document in a collection
+export async function updateEntry(collectionName, id, updatedData) {
+  try {
+    const docRef = doc(database, collectionName, id);
+    await updateDoc(docRef, updatedData);
+  } catch (error) {
+    console.error('Error updating document: ', error);
+  }
+}
+
+// Function to delete a document from a collection
+export async function deleteEntry(collectionName, id) {
+  try {
+    const docRef = doc(database, collectionName, id);
+    await deleteDoc(docRef);
+  } catch (error) {
+    console.error('Error deleting document: ', error);
   }
 }
